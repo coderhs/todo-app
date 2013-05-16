97fc8dcab626f3bc4d2c65a8908dd8e709d4e32a
coderhs <mailme@hsps.in>
2013-05-16 15:09:16 +0530

    Initial Commit - Have added site controller

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def download
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '28ced9b80ca0602a242cb82400fef141bc9e906a6a367272cf6db8ed067341909e4330ebecf8914b51e80584cf1182f3f22800565a6f42239181c4f4960639cf'
